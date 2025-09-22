bc4d3288e836f4b9bfdb88c33c856b2bf5004cae
Robert <robertoctm10@gmail.com>
2025-09-22 02:10:26 -0300

    establecimiento del formato de registro (divisiones)

--- IPC_Analyzer/funciones.h
+++ IPC_Analyzer/funciones.h
@@ -3,4 +3,30 @@
 
 
 
+typedef struct
+{
+    char cod[21];
+    char descrip[31];
+    char clasi[31];
+    double float ind_ipc;
+    double float v_m_ipc;
+    double float V_i_a_ipc;
+    char region[10];
+    int periodo_codif;
+
+}Division;
+
+
+//Primitivas
+Division* divisionDecodificarFecha(Division*);
+
+
+
+
+//primitivasn't
+
+
+
+
+
 #endif // FUNCIONES_H_INCLUDED
