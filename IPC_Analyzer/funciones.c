ceecdd21a42852713feb7d589b7c3732ddcaa070
MrRed-Fish <alexisnahuelsandes97@gmail.com>
2025-10-19 15:20:24 -0300

    Para destruir, hay que hacer...

--- IPC_Analyzer/funciones.c
+++ IPC_Analyzer/funciones.c
@@ -17,206 +17,6 @@ char* dirUltComillas(char *);
 int setearString(char *,char *);
 int setearDouble(char *, double *);
 
-
-
-
-
-
-int convertirFechaDecodificadaAString(DIVISION* reg)//P2
-{
-    char *formatoPerido = reg -> periodo_codif.periodo,
-         anioStr[20]; //vec para guardar anio(int) como anioStr(string)
-
-    int mes = reg->periodo_codif.mes,
-        anio = reg->periodo_codif.anio;
-
-    char matMes[MESES][CANTMAXCHAR] = {{"Enero"},{"Febrero"},{"Marzo"},{"Abril"},
-                                       {"Mayo"},{"Junio"},{"Julio"},{"Agosto"},
-                                       {"Septiembre"},{"Octubre"},{"Noviembre"},{"Diciembre"}
-                                      };
-    //convertir anio(int) a anioStr(string)
-    sprintf(anioStr, "%d", anio);
-
-    strcpy(formatoPerido,*(matMes+(mes-1))); //Enero
-    strcat(formatoPerido, " - ");            //Enero -
-    strcat(formatoPerido, anioStr);          //Enero - 2017
-
-    return TODO_OK;
-}
-
-void filtroRegClaf(DIVISION* reg, char* desc, char *regi)//P3
-{
-    if(strcmpi(reg -> clasif, desc) == 0 && strcmpi(reg -> region, regi) == 0)
-        printf("\n\n%20s %30s %30s %8.2lf %5.2lf %7.2lf      %9s %4d%2d %s",reg->cod,
-               reg->descrip,reg->clasif,reg->ind_ipc,reg->v_m_ipc,reg->v_i_a_ipc,
-               reg->region,reg->periodo_codif.anio,reg->periodo_codif.mes,reg->periodo_codif.periodo);
-}
-
-
-/*void divisionParseReg()
-{
-    FILE *archTxt = fopen("../Data/serie_ipc_divisiones(test).csv","r+t");
-
-    FILE *archBin = fopen("../Data/serie_ipc_divisiones(test).dat", "wb"); //nuevo arch bin
-
-    char registro[MAXTAMREG];
-
-
-    if(!archTxt)
-    {
-        puts("No se pudo abrir el archivo: serie_ipc_divisiones(test)");
-        exit(1);
-    }
-
-    fgets(registro,MAXTAMREG,archTxt); //saltar encabezado
-
-    while(fgets(registro,MAXTAMREG,archTxt)) //fgets siempre trae un registro a variable con \0 al final
-    {
-        printf("\n\nantes ---------------------------------");
-        printf("\n%s",registro);
-
-        comaApunto(registro, 4); //sabiendo la estructura y como estan divididos
-        comaApunto(registro, 5); //
-
-        reemplazoNAx0(registro, 5); //sabiendo la estructura y como estan divididos
-        reemplazoNAx0(registro, 6); //
-
-        printf("despues +++++++++++++++++++++++++++++++"); //valido lo del ultimo reg
-        printf("\n%s\n\n",registro);
-
-         comment ya debatido¿
-        despues de tener la variable registro parseada, puedo
-        sobreescribirla en la misma iteracion, parseando el arch
-        completo (aun no, para no malograr el arch pq hay q revisar
-        si todas las funciones se bancan el archivo
-
-        fseek(archTxt, -sizeof(registro), SEEK_SET); //SEEK_CUR***
-        fprintf(archTxt, "%s\n", registro);
-        fflush(arch);
-
-
-        idea principal:
-            a medida que va reemplazando la coma a punto, y NA a cero,
-            vamos parseando y guardando como estructura dentro de un .bin
-            paralelo al csv, asi después trabajamos solo en ese archivo.
-            y, si la situación lo amerita en algun futuro, convertimos
-            ese bin a csv y listo, sino nel
-
-        DIVISION regB;
-
-        regTextABin(&regB, registro);
-
-        fwrite(&regB, sizeof(DIVISION), 1, archBin); //vamos guardando en nuestro nuevo archivito
-    }
-
-    fclose(archBin);
-    fclose(archTxt);
-}*/
-
-/*void comaApunto(char *pInf, int lac)
-{
-    //nos desplazamos en el str registro por los separadores
-    //pInf(inferior/inicio del campo), pSup(superior/fin del campo)
-    char *pSup,
-         *finReg = strchr(pInf,'\n');
-    int numCampo;
-
-    //archivo sin \n (solo \0) en el ultimo registro -> registro valido
-    if(finReg != NULL)
-        *finReg = '\0';
-
-    //indice_IPC campo nro 4
-    for(numCampo = 1 ; numCampo < lac ; numCampo++)
-        pInf = strchr(pInf + numCampo,';'); //pInf en el separador antes del campo clave
-
-    pSup = strchr(pInf+1,';'); //pInf en el separador despues del campo clave
-
-    //paso de , a .
-    while(pInf != pSup && *pInf != ',')
-        pInf++;
-
-    if(pInf != pSup)
-        *pInf = '.';
-}*/
-
-
-/*void regTextABin(DIVISION *regB, char *regT)
-{
-    char *pf;
-    int aux;
-
-    pf = strrchr(regT, ';');
-    if (pf != NULL) //fecha
-    {                                      //pf+2 para quitar las comillas iniciales
-        strncpy(regB->periodo_codif.periodo, pf + 2,sizeof(regB->periodo_codif.periodo) - 1);
-        regB->periodo_codif.periodo[sizeof(regB->periodo_codif.periodo)-11]= '\0';
-                                                            //-11: hardcodeo pa quitar  las comillas finales
-        //atoi = string to int (string a pasar)!!!!!!!!!!!! ASCII to Integer
-        aux = atoi(regB->periodo_codif.periodo); //aux=9283 93 (ej)
-        regB->periodo_codif.anio= aux/100; // anio= 9283
-        regB->periodo_codif.mes= aux%100; // anio= 93
-
-        *pf='\0';
-    }
-
-    pf= strrchr(regT, ';');
-    if(pf!= NULL) //region
-    {
-        strncpy(regB->region, pf+1, sizeof(regB->region) - 1);
-        regB->region[sizeof(regB->region)-1]= '\0';
-        *pf='\0';
-    }
-
-    pf= strrchr(regT, ';');
-    if(pf!=NULL)
-    {                   //strtod: string to double (string, NULL)
-        regB->v_i_a_ipc = strtod(pf+1, NULL);
-        *pf='\0';
-    }
-
-    pf= strrchr(regT, ';');
-    if(pf!=NULL)
-    {                   //strtod: string to double (string, NULL)
-        regB->v_m_ipc = strtod(pf+1, NULL);
-        *pf='\0';
-    }
-
-    pf= strrchr(regT, ';');
-    if(pf!=NULL)
-    {                   //strtod: string to double (string, NULL)
-        regB->ind_ipc = strtod(pf+1, NULL);
-        *pf='\0';
-    }
-
-    pf= strrchr(regT, ';');
-    if(pf!=NULL) //clasificacion
-    {
-        strncpy(regB->clasif, pf+1, sizeof(regB->clasif) - 1);
-        regB->clasif[sizeof(regB->clasif)-1]= '\0';
-        *pf='\0';
-    }
-
-    pf= strrchr(regT, ';');
-    if(pf!=NULL) //descripcion
-    {
-        strncpy(regB->descrip, pf+1, sizeof(regB->descrip) - 1);
-        regB->descrip[sizeof(regB->descrip)-1]= '\0';
-        *pf='\0';
-    }
-
-    //ya llego al inicio del regT pal cod
-    strncpy(regB->cod, regT, sizeof(regB->cod) - 1);
-    regB->cod[sizeof(regB->cod)-1]= '\0';
-
-
-    puts("cargao");
-}*/
-
-
-
-
-
-
 int divisionesArchTextABin()
 {
     FILE *archTxt = fopen("../Data/serie_ipc_divisiones(test).csv","rt");
@@ -251,11 +51,11 @@ int divisionesArchTextABin()
     fclose(archBin);
     fclose(archTxt);
 
-    puts("TODO_OK");
+    puts("\nTODO_OK\n");
 
     return TODO_OK;
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int regTextABin(DIVISION *regB, char *regT)
 {
     char *pFinCamp, *pIniCamp;
@@ -284,16 +84,11 @@ int regTextABin(DIVISION *regB, char *regT)
 
      setearDouble(pIniCamp,&regB -> v_i_a_ipc);
 
-     //*(dirUltComillas(regT)) = '\0';
-
-
      pFinCamp = strrchr(regT,';'); //v_m_IPC
      *pFinCamp = '\0';
      pIniCamp = strrchr(regT,';') + 1;
 
      setearDouble(pIniCamp,&regB -> v_m_ipc);
-
-     //*(dirUltComillas(regT)) = '\0';
 
      pFinCamp = strrchr(regT,';'); //Indice_IPC
      *pFinCamp = '\0';
@@ -310,36 +105,20 @@ int regTextABin(DIVISION *regB, char *regT)
 
      *(dirUltComillas(regT)) = '\0';
 
-
-
-
-
-
-     /*pFinCamp = strrchr(regT,';'); //descripcion
-     *(dirUltComillas(regT)) = '\0';
-
-     pIniCamp = dirUltComillas(regT) + 1;
-
-     normalizarDescr(pIniCamp,regB);
-
-     *(dirUltComillas(regT)) = '\0';*/
-
-
-    pFinCamp = strrchr(regT,';'); // ERROR, el ultimo # tiene en el campo "descripcion"  o consideramos
-     *pFinCamp = '\0';            // que no pude tener " al inicio y al fin
-     pIniCamp = strrchr(regT,';') + 1;
-
-     normalizarDescr(pIniCamp,regB);
-    *(dirUltComillas(regT)) = '\0';
-
-
-
-
-
-
-
-
-
+     pFinCamp = strrchr(regT, ';'); //version agregada de la descripcion
+     *pFinCamp = '\0';
+     pIniCamp = strrchr(regT, ';');
+     if(*(pIniCamp + 1) == '"')//Evaluo si tiene comillas o si es el NA
+     {
+         *(pFinCamp - 1) = '\0';
+         normalizarDescr(pIniCamp + 2,regB);
+         *(dirUltComillas(regT)) = '\0';
+     }
+     else
+     {
+         normalizarDescr(pIniCamp + 1, regB);
+         *pIniCamp = '\0';
+     }
 
      pFinCamp = strrchr(regT,';'); //codigo
      *(dirUltComillas(regT)) = '\0';
@@ -348,9 +127,7 @@ int regTextABin(DIVISION *regB, char *regT)
 
      setearString(pIniCamp,regB -> cod);
 
-
      return TODO_OK;
-
 }
 
 char* dirUltComillas(char *TrozoRegT)
@@ -392,6 +169,28 @@ int divisionDecodificarFecha(char* fechaStr, DIVISION* regB)
     return TODO_OK;
 }
 
+int convertirFechaDecodificadaAString(DIVISION* reg)//P2
+{
+    char *formatoPerido = reg -> periodo_codif.periodo,
+         anioStr[20]; //vec para guardar anio(int) como anioStr(string)
+
+    int mes = reg->periodo_codif.mes,
+        anio = reg->periodo_codif.anio;
+
+    char matMes[MESES][CANTMAXCHAR] = {{"Enero"},{"Febrero"},{"Marzo"},{"Abril"},
+                                       {"Mayo"},{"Junio"},{"Julio"},{"Agosto"},
+                                       {"Septiembre"},{"Octubre"},{"Noviembre"},{"Diciembre"}
+                                      };
+    //convertir anio(int) a anioStr(string)
+    sprintf(anioStr, "%d", anio);
+
+    strcpy(formatoPerido,*(matMes+(mes-1))); //Enero
+    strcat(formatoPerido, " - ");            //Enero -
+    strcat(formatoPerido, anioStr);          //Enero - 2017
+
+    return TODO_OK;
+}
+
 int setearString(char *cadena,char *campo)
 {
     strcpy(campo,cadena);
@@ -412,22 +211,10 @@ int setearDouble(char *cadena, double *campo)
     return TODO_OK;
 }
 
-int comaApunto(char *cadena)
-{
-    //paso de , a .
-    while(*cadena != '\0' && *cadena != ',')
-        cadena++;
-
-    if(*cadena == ',')
-        *cadena = '.';
-
-    return TODO_OK;
-}
-
 int normalizarDescr(char *cadena, DIVISION* reg)
 {
     int largoDeLaCadena = strlen(cadena);
-    printf("\nLargo de la cadena: %d", largoDeLaCadena);
+    //printf("\nLargo de la cadena: %d", largoDeLaCadena);
 
     if (largoDeLaCadena == 0) {
         printf("\nError: cadena vacia");
@@ -450,8 +237,30 @@ int normalizarDescr(char *cadena, DIVISION* reg)
     // Copiar al registro
     strcpy(reg->descrip, temp);
 
-    printf("\nDescripción normalizada: %s", reg->descrip);
+    //printf("\nDescripción normalizada: %s", reg->descrip);
     return TODO_OK;
 }
+
+void filtroRegClaf(DIVISION* reg, char* desc, char *regi)//P3
+{
+    if(strcmpi(reg -> clasif, desc) == 0 && strcmpi(reg -> region, regi) == 0)
+        printf("\n\n%20s %30s %30s %8.2lf %5.2lf %7.2lf      %9s %4d%2d %s",reg->cod,
+               reg->descrip,reg->clasif,reg->ind_ipc,reg->v_m_ipc,reg->v_i_a_ipc,
+               reg->region,reg->periodo_codif.anio,reg->periodo_codif.mes,reg->periodo_codif.periodo);
+}
+
+int comaApunto(char *cadena)
+{
+    //paso de , a .
+    while(*cadena != '\0' && *cadena != ',')
+        cadena++;
+
+    if(*cadena == ',')
+        *cadena = '.';
+
+    return TODO_OK;
+}
+
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- IPC_Analyzer/main.c
+++ IPC_Analyzer/main.c
@@ -17,67 +17,14 @@ void mostrarBin();
 
 int main()
 {
-    //divisionParseReg();
 
     divisionesArchTextABin();
-    system("pause");
 
-
-    /*int i;
-    DIVISION reg[TAM] ={
-    {"023", "NIVEL GENERAL", "Nivel general", 150.01, 16.0, 18.5, "GBA",{9741,49,""}},
-    {"045", "NIVEL GENERAL", "Nivel general y divisiones", 100.5, 90.0, 0.1, "Nacional",{9741,49}},
-    {"x23", "bebidas alcohólicas", "Nivel general y COICOP", 140, 0.1, 10.0, "Pampeana",{9741,49}},
-    {"11", "Bebidas y tabaco", "COICOP", 11, 500, 70, "Noroeste",{9741,49}},
-    {"03", "Prendas de vestir", "divisiones COICOP", 70, 0, 14, "Cuyo",{9741,49}},
-    {"04", "Vivienda, electricidad, gas ", "Nivel general", 80, 0, 14, "Patagonia",{9741,49}},
-    {"35", "NIVEL GENERAL", "COICOP", 101.313, 1.3, 0, "GBA",{9743,74}},
-    {"abc", "bebidas no alcoholicas", "COICOP", 101.3069, 1.3, 14, "GBA",{9743,74}},
-    {"04", "gas y otros combustibles", "Nivel COICOP", 110.3184, 10.3, 19, "Patagonia",{9743,74}},
-    {"xd", "NA", "Categorias", 101.6969, 1.7,7.1, "Nacional",{9743,74}}
-    };
-
-    //imprimirRegistros(reg);
-
-    for(i = 0;i<TAM;i++)
-    {
-        divisionDecodificarFecha(reg+i);
-    }
-
-    imprimirRegistros(reg);
-
-    printf("\n\n\n\n%20s %30s %30s %13s %9s %11s %11s %8s",
-           "CODIGO","DESCRIPCION","CLASIFICADOR",
-           "INDICE_IPC","V_M_IPC","V_I_A_IPC","REGION","PERIODO");
-    for(i = 0;i<TAM;i++)
-    {
-        convertirFechaDecodificadaAString(reg+i);
-        normalizarDescr(reg+i);
-        filtroRegClaf(reg+i,"COICOP", "GBA");
-    }*/
+    mostrarBin();
 
     return 0;
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void imprimirRegistros(DIVISION* reg)
-{
-    int i;
-
-    printf("\n\n\n\n%20s %30s %30s %13s %9s %11s %11s %8s",
-           "CODIGO","DESCRIPCION","CLASIFICADOR",
-           "INDICE_IPC","V_M_IPC","V_I_A_IPC","REGION","PERIODO");
-
-    for(i=0;i<TAM;i++)
-    {
-        printf("\n\n%20s %30s %30s %8.2lf %5.2lf %7.2lf      %9s %4d%2d %s",reg->cod,
-               reg->descrip,reg->clasif,reg->ind_ipc,reg->v_m_ipc,reg->v_i_a_ipc,
-               reg->region,reg->periodo_codif.anio,reg->periodo_codif.mes,reg->periodo_codif.periodo);
-
-        reg++;
-    }
-}
-//si hago %4d%2d para la fecha, podria separar los digitos en 2 campos diferentes o una variable de tipo estructura anidada
-
 void mostrarBin()
 {
     FILE* pf = fopen("../Data/serie_ipc_divisiones(test).dat", "rb");
@@ -99,71 +46,7 @@ void mostrarBin()
         puts("------------------------------------------------------------\n");
     }
 
-
     fclose(pf);
     system("pause");
 }
 
-
-
-
-
-
-
-
-
-
-
-
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-#include "funciones.h"
-
-
-int main()
-{
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
-
-
-    FILE *pf = fopen("../Data/serie_ipc_divisiones(test).csv","r+t");
-    DIVISION reg;
-
-    if(pf == NULL)
-    {
-        printf("\nERROR\n");
-        exit(1);
-    }
-
-
-    typedef struct
-{
-    char cod[21];
-    char descrip[31];
-    char clasif[31];
-    double ind_ipc;
-    double v_m_ipc;
-    double v_i_a_ipc;
-    char region[10];
-    FECHA periodo_codif; //asi tenemos separada la fecha(capaz lo necesitemos mas adelante)
-    }DIVISION;
-
-    fread(&reg,sizeof(DIVISION),1,pf);
-    while(!feof(pf))
-    {
-        fscanf(pf,"%s;%s;%s;%f;%f;%f;%s;%d",reg.cod,reg.descrip,reg.clasif,&reg.ind_ipc,&reg.v_m_ipc,&reg.v_i_a_ipc,reg.region,&reg.periodo_codif);
-
-        printf("%s %s %s %f %f %f %s %d\n",reg.cod,reg.descrip,reg.clasif,reg.ind_ipc,reg.v_m_ipc,reg.v_i_a_ipc,reg.region,reg.periodo_codif);
-
-
-
-        //printf("%s \n\n", registro.cod);
-        fread(&reg,sizeof(DIVISION),1,pf);
-    }
-
-
-
-
-
-}*/
